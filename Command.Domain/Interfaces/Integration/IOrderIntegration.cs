b7a9cf8fd91b79a5a3b047d811a643b6c0403377
Rafael Araujo <raff-araujo@hotmail.com>
2024-05-12 05:33:34 -0300

    Adicionando API de Comandas

---
+++ Command.Domain/Interfaces/Integration/IOrderIntegration.cs
@@ -0,0 +1,9 @@
+﻿using Command.Domain.Dtos;
+
+namespace Command.Domain.Interfaces.Integration
+{
+	public interface IOrderIntegration
+	{
+		Task<IEnumerable<OrderDto>> GetOrdersByCommand(int commandId, string accessToken);
+	}
+}
---
+++ Command.Service/Integration/OrderIntegration.cs
@@ -0,0 +1,29 @@
+﻿using Command.Domain.Dtos;
+using Command.Domain.Interfaces.Integration;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Command.Service.Integration
+{
+	public class OrderIntegration(IConfiguration configuration) : IOrderIntegration
+	{
+		private readonly IConfiguration _configuration = configuration;
+		private readonly HttpClient _httpClient = new HttpClient();
+
+		public async Task<IEnumerable<OrderDto>> GetOrdersByCommand(int commandId, string accessToken)
+		{
+			var baseUrl = _configuration.GetSection("OrderApi:UrlBase").Value;
+			var url = @$"{baseUrl}get-orders-command?CommandId={commandId}";
+			_httpClient.BaseAddress = new Uri(url);
+			_httpClient.DefaultRequestHeaders.Accept.Clear();
+			_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+			var response = await _httpClient.GetAsync(url);
+			var jsonString = await response.Content.ReadAsStringAsync();
+			var orders = JsonConvert.DeserializeObject<IEnumerable<OrderDto>>(jsonString);
+			return orders;
+		}
+	}
+}
