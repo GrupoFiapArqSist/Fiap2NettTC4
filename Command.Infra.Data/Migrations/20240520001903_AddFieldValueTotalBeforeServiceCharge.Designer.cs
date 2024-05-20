df707775866ad7c8107ca1c9a146cef62dc75337
Rafael Araujo <raff-araujo@hotmail.com>
2024-05-19 21:21:51 -0300

    Add migration field ValueTotalBeforeServiceCharge

--- Command.Infra.Data/Migrations/20240520001903_AddFieldValueTotalBeforeServiceCharge.Designer.cs
+++ Command.Infra.Data/Migrations/20240520001903_AddFieldValueTotalBeforeServiceCharge.Designer.cs
@@ -4,6 +4,7 @@ using Command.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Command.Infra.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240520001903_AddFieldValueTotalBeforeServiceCharge")]
+    partial class AddFieldValueTotalBeforeServiceCharge
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Command.Infra.Data/Migrations/20240520001903_AddFieldValueTotalBeforeServiceCharge.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Command.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldValueTotalBeforeServiceCharge : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceChage",
+                table: "Command");
+
+            migrationBuilder.RenameColumn(
+                name: "ValueTotal",
+                table: "Command",
+                newName: "ValueTotalBeforeServiceCharge");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ValueTotalBeforeServiceCharge",
+                table: "Command",
+                newName: "ValueTotal");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ServiceChage",
+                table: "Command",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
